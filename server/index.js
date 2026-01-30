bef1c1c2a9d13292b9402d240825d8539fe87d59
Benaya Chessa <126884528+benayachessa@users.noreply.github.com>
2026-01-30 22:20:16 +0700

    Config: Siap Deploy ke Vercel (Backend)

--- server/index.js
+++ server/index.js
@@ -18,3 +18,12 @@ app.use("/expenses", expenseRoutes);
 app.listen(port, () => {
   console.log(`Server lari kencang di port ${port}`);
 });
+
+module.exports = app;
+
+if (process/env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server lari kencang di port ${port}`);
+  });
+}
