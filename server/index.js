dccdf3cb88a55d1a9f6037300f43fa060fa766eb
Benaya Chessa <126884528+benayachessa@users.noreply.github.com>
2026-01-30 23:31:29 +0700

    Final Fix: Backend Ready For Deployment

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.listen(port, () => {
 
 module.exports = app;
 
-if (process/env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   const port = process.env.PORT || 5000;
   app.listen(port, () => {
     console.log(`Server lari kencang di port ${port}`);
