1f382ed8cee245ec63deee559da9ca6d6a90997b
Benaya Chessa <126884528+benayachessa@users.noreply.github.com>
2026-01-30 23:05:58 +0700

    Config: Siap Deploy ke Vercel (Backend)v3

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "process.env.DB_HOST",
-  user: "process.env.DB_USER",
-  password: "process.env.DB_PASSWORD",
-  database: "process.env.DB_NAME",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
